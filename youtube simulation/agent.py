14f64226b7af8078b350caaf5bf4993a93c546bd
Sasha R <89604329+arabeno@users.noreply.github.com>
2023-07-04 20:58:53 -0400

    Still working on it; commented out a bunch

--- youtube simulation/agent.py
+++ youtube simulation/agent.py
@@ -27,64 +27,66 @@ class Agent:
 
 
 
-a = Agent()
-print("Is this person watching a video currently?: " + str(a.is_watching())) # the person is not watching by default
-a.watching() # the person clicks on a video
-print("Is this person watching a video currently?: " + str(a.is_watching())) # the person is now watching
 
 
-# Now, what we want to do is simulate how this idea spreads through a population. 
-# Say that there are N people in the population, and each day each person talks to k random other people. 
-# If someone talks to someone else who is watching a video, then the new person also begins to watch a video 
-# with probability p. 
-# We’ll start with one watching person and keep track of how the idea spreads
-
-N = 1000 # population size
-k = 5     # number of people each person talks to
-p = 0.1   # probability that someone is watching a video
-
-pop = [Agent() for i in range(N)] # our population
-pop[0].watching()
+# a = Agent()
+# print("Is this person watching a video currently?: " + str(a.is_watching())) # the person is not watching by default
+# a.watching() # the person clicks on a video
+# print("Is this person watching a video currently?: " + str(a.is_watching())) # the person is now watching
 
 
-# now let’s write a function that will simulate all interactions in a day
+# # Now, what we want to do is simulate how this idea spreads through a population. 
+# # Say that there are N people in the population, and each day each person talks to k random other people. 
+# # If someone talks to someone else who is watching a video, then the new person also begins to watch a video 
+# # with probability p. 
+# # We’ll start with one watching person and keep track of how the idea spreads
 
-for i in range(N):
-    if pop[i].is_watching():
-        # person i tells all their contacts to watch a video
-        contacts = randint(N, size=k)
-        for j in contacts:
-            if rand() < p:
-                pop[j].watching()
+# N = 1000 # population size
+# k = 5     # number of people each person talks to
+# p = 0.1   # probability that someone is watching a video
 
-
-# we want to count how many people are watching a video at the end of the day
-def count_watching(pop):
-    return sum(p.is_watching() for p in pop)
-
-print(count_watching(pop))
+# pop = [Agent() for i in range(N)] # our population
+# pop[0].watching()
 
 
-# now let’s see how the watching population changes over time
-T = 100 # number of days to simulate
-counts = [count_watching(pop)]
-for t in range(T):
-    # update the population
-    for i in range(N):
-        if pop[i].is_watching():
-            # person i enlightens all their contacts
-            contacts = randint(N, size=k)
-            for j in contacts:
-                if rand() < p:
-                    pop[j].watching()
+# # now let’s write a function that will simulate all interactions in a day
+
+# for i in range(N):
+#     if pop[i].is_watching():
+#         # person i tells all their contacts to watch a video
+#         contacts = randint(N, size=k)
+#         for j in contacts:
+#             if rand() < p:
+#                 pop[j].watching()
+
+
+# # we want to count how many people are watching a video at the end of the day
+# def count_watching(pop):
+#     return sum(p.is_watching() for p in pop)
+
+# print(count_watching(pop))
+
+
+# # now let’s see how the watching population changes over time
+# T = 100 # number of days to simulate
+# counts = [count_watching(pop)]
+# for t in range(T):
+#     # update the population
+#     for i in range(N):
+#         if pop[i].is_watching():
+#             # person i enlightens all their contacts
+#             contacts = randint(N, size=k)
+#             for j in contacts:
+#                 if rand() < p:
+#                     pop[j].watching()
                     
-    # add to our counts
-    counts.append(count_watching(pop))
+#     # add to our counts
+#     counts.append(count_watching(pop))
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-plt.plot(counts)
-plt.show()
+# plt.plot(counts)
+# plt.show()
 
 
 
