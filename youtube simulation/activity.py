cc17941ff711b11da887d219c33bf296d4a4ad77
Sasha R <89604329+arabeno@users.noreply.github.com>
2023-07-19 00:13:16 -0400

    Further defined watch() function

--- youtube simulation/activity.py
+++ youtube simulation/activity.py
@@ -1,5 +1,6 @@
 import numpy as np
 from video import Video
+import random
 
 class Activity:
     """ Class which defines Activities within the YouTube simulation."""
@@ -20,11 +21,37 @@ class Activity:
     def give_thumbs_up(video):
         video.thumbs_up += 1; # this works! hooray!
 
+
+
     def watch(video):
         # Increment how many views the video got
         video.views += 1;
-        # Add the video object to the patient's log
-        # video.log.append(video);
+        # Add the video length to total time spent watching today
+
+
+        # The below information has to be collected outside of the function
+        # thumbs_up_given_today = 0
+        # thumbs_up_skipped_today = 0
+
+        # Coin flip to determine if give_thumbs_up is called
+        coin_flip = random.choice([0,1])
+        if(coin_flip):
+            Activity.give_thumbs_up(video)
+            print("Thumbs up given")
+            # thumbs_up_given_today += 1
+        else:
+            print("No thumbs up given")
+            # thumbs_up_skipped_today += 1
+
+        # print(thumbs_up_given_today)
+        # print(thumbs_up_skipped_today)
+
+    
+    def random_vid(list_of_vids):
+        return random.choice(list_of_vids)
+
+
+
 
 
 
