4e85b8b05336e60b74bbf28a2e63c4dd86ab41dc
Sasha R <89604329+arabeno@users.noreply.github.com>
2023-07-31 23:11:30 -0400

    Trying to figure out why middle agents aren't watching any videos

--- youtube simulation/behavior_reference.py
+++ youtube simulation/behavior_reference.py
@@ -40,52 +40,52 @@ BEHAVIOR_ARCHETYPE_PARAMETERS = {
         "yt_time_threshold": 120,       #will spend up to two hours on YouTube per day
         "political_affiliation": "left",
         "video_extremity": 1.0,         #will watch videos that rate high on extreme-ness
-        "popularity_threshold": 20000   #will watch any video with at least 20,000 views
+        "popularity_threshold": 2000   #will watch any video with at least 2000 views
     },
 
     # Agent is willing to watch longer videos aligned with their beliefs, but not as long as the progressive activist.
     "traditional_liberal": {
         "longest_vid_threshold": 60,    #will watch videos up to 1 hour long
-        "yt_time_threshold": 60,        #will spend up to one hour per day on YouTube   
+        "yt_time_threshold": 80,        #will spend up to one hour per day on YouTube   
         "political_affiliation": "left",
         "video_extremity": 0.8,         #will watch some extreme videos, but not much
-        "popularity_threshold": 50000   #will watch any video with at least 50,000 views
+        "popularity_threshold": 50000   #will watch any video with at least 10,000 views
     },
 
     # Agent will spend a little longer than average watching videos, but nothing incredibly extreme.
     "passive_liberal": {
         "longest_vid_threshold": 40,    #will watch videos up to 40 minutes long
-        "yt_time_threshold": 50,        #will spend 50 minutes per day on YouTube
+        "yt_time_threshold": 60,        #will spend 50 minutes per day on YouTube
         "political_affiliation": "middle",
         "video_extremity": 0.6,         #will not watch anything too extreme  
-        "popularity_threshold": 10000  #watches videos with at least 100k views
+        "popularity_threshold": 100000  #watches videos with at least 100k views
     },
 
     # Agent doesn't want to watch long videos. Will engage with popular, non-partisan content.
     "politically_disengaged": {
         "longest_vid_threshold": 30,    #won't watch any video longer than 15 minutes
-        "yt_time_threshold": 40,        #will spend at most 40 minutes per day on YouTube (national average)
+        "yt_time_threshold": 60,        #will spend at most 40 minutes per day on YouTube (national average)
         "political_affiliation": "middle",
         "video_extremity": 0.5,         #does not want to watch videos that are extreme or partisan
-        "popularity_threshold": 10000  #watches more popular videos; at least 500k views
+        "popularity_threshold": 500000  #watches more popular videos; at least 500k views
     },
 
     # Agent will spend a little longer than average watching videos, but nothing incredibly extreme.
     "moderate": {
         "longest_vid_threshold": 40,    #will watch videos up to 40 minutes long
-        "yt_time_threshold": 50,        #will spend 50 minutes per day on YouTube
+        "yt_time_threshold": 60,        #will spend 50 minutes per day on YouTube
         "political_affiliation": "middle",
         "video_extremity": 0.4,         #will not watch anything too extreme  
-        "popularity_threshold": 50000  #watches videos with at least 50k views
+        "popularity_threshold": 100000  #watches videos with at least 50k views
     },
 
     # Agent is willing to watch longer videos aligned with their beliefs, but not as long as the devoted conservative.
     "traditional_conservative": {
         "longest_vid_threshold": 60,    #will watch videos up to 1 hour long
-        "yt_time_threshold": 60,        #will spend up to one hour per day on YouTube   
+        "yt_time_threshold": 80,        #will spend up to one hour per day on YouTube   
         "political_affiliation": "right",
         "video_extremity": 0.2,         #will watch some extreme videos, but not much
-        "popularity_threshold": 10000   #will watch any video with at least 50,000 views
+        "popularity_threshold": 50000   #will watch any video with at least 10,000 views
     },
 
     # Agent is willing to spend lots of time watching more "underground" and less-popular videos, for longer.
@@ -95,7 +95,7 @@ BEHAVIOR_ARCHETYPE_PARAMETERS = {
         "yt_time_threshold": 120,       #will spend up to two hours on YouTube per day
         "political_affiliation": "right",
         "video_extremity": 0.0,         #will watch videos that rate high on extreme-ness
-        "popularity_threshold": 20000   #will watch any video with at least 20,000 views
+        "popularity_threshold": 2000   #will watch any video with at least 2000 views
     },
     
 
